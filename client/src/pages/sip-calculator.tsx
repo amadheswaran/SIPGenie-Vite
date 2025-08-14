d4b85831bf64d495298f0ee7411b5919e81accaa
Arunkumar Madheswaran <43840802+amadheswaran@users.noreply.github.com>
2025-08-14 16:45:48 +0530

    Add the entire section for PDF export

--- client/src/pages/sip-calculator.tsx
+++ client/src/pages/sip-calculator.tsx
@@ -25,7 +25,7 @@ export default function SIPCalculatorPage() {
       </section>
 
       {/* Main Calculator Section */}
-      <section className="py-16">
+      <section id="pdf-export-section" className="py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <SIPCalculator />
         </div>
